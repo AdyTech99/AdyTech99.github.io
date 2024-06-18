735dd0995e49ff5800b82fd052ea373f444fb8ea
Swaritz <74529440+Swaritz@users.noreply.github.com>
2024-06-18 15:43:28 +0530

    Updated script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 'use strict';
+const blob = document.getElementById("blob");
+window.onpointermove = event => {
+    const { pageX, pageY } = event;
+
+    blob.animate({
+        left: `${pageX}px`,
+        top: `${pageY}px`
+    }, { duration: 300, fill: "forwards" });
+}
+
 
 
 
