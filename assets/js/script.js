26782a9bcee0d1ed941ac88a29a625b1c08df185
SWARIT13 <swarit.kr9@gmail.com>
2024-06-21 18:49:12 +0530

    Fixed rensposiveness on mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,9 @@ window.onpointermove = event => {
         top: `${pageY}px`
     }, { duration: 300, fill: "forwards" });
 }
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(navigator.userAgent)) {
+    $('remover').remove();
+}
 
 function isMacOS() {
     return navigator.platform.toUpperCase().indexOf('MAC') >= 0;
